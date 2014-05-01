bf277cf63521c506183d78e24647390a3d59f731
Gareth Williams <gareth@garethw.net>
2014-05-01 16:21:16 +0100

    First commit

---
+++ LowRezJam/app.ts
@@ -0,0 +1,29 @@
+﻿/// <reference path="phaser.d.ts"/>
+class SimpleGame {
+
+    constructor() {
+        this.game = new Phaser.Game(640, 640, Phaser.CANVAS, 'content', { preload: this.preload, create: this.create }, false, false);
+    }
+
+    game: Phaser.Game;
+
+    preload() {
+        this.game.load.image('mock', 'img/lowrez.png');
+        this.game.stage.smoothed = false;
+        this.game.canvas.getContext("2d").msImageSmoothingEnabled = false;
+    }
+
+    create() {
+        var logo = this.game.add.sprite(0, 0, 'mock');
+        logo.anchor.setTo(0, 0);
+        logo.scale.x = 20.0;
+        logo.scale.y = 20.0;
+    }
+
+}
+
+window.onload = () => {
+
+    var game = new SimpleGame();
+
+};
